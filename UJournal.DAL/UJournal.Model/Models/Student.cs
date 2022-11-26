295144aaf4aad2ea9145a1a86932e6226be9712a
Andriy-KAP <andriykuklinov@gmail.com>
2022-11-27 01:33:17 +0200

    Student model.

---
+++ UJournal.DAL/UJournal.Model/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UJournal.Model.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; } = "";
+        [Required]
+        public string LastName { get; set; } = "";
+        [Required]
+        public string PhoneNumber { get; set; } = "";
+        [Required]
+        public DateTime BirthDate { get; set; }
+    }
+}
