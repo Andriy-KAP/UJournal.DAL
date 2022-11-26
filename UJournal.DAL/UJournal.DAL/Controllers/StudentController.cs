8894973d66490f79321a30bec52358ee36ac7429
Andriy-KAP <andriykuklinov@gmail.com>
2022-11-27 01:34:51 +0200

    Configuring EF Core(connecion string).

---
+++ UJournal.DAL/UJournal.DAL/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using UJournal.Model.Core;
+using UJournal.Model.Models;
+
+namespace UJournal.DAL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private UJournalContext context;
+        public StudentController(UJournalContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+
+            return this.context.Students.ToList<Student>();
+        }
+        
+    }
+}
---
+++ UJournal.DAL/UJournal.DAL/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using UJournal.Model.Core;
+using Microsoft.Extensions.Configuration;
+namespace UJournal.DAL
+{
+    public class Startup
+    {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<UJournalContext>(options =>
+            {
+                options.UseSqlServer(this.Configuration.GetConnectionString("UJournal"),
+                    sqlServerOptionsAction: sqlOptions =>
+                    {
+                        sqlOptions.EnableRetryOnFailure(
+                            maxRetryCount: 5,
+                            maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null
+                        );
+                    }    
+                );
+            });
+            services.AddControllers();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapControllers();
+            });
+        }
+
+        
+    }
+}
