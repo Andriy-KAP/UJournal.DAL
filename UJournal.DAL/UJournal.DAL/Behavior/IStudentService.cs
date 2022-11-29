9a436246ce1c574423eae030c3fd2031fbd5197d
Andriy-KAP <andriykuklinov@gmail.com>
2022-11-29 15:13:40 +0200

    Implementing student service.

---
+++ UJournal.DAL/UJournal.DAL/Behavior/IStudentService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using UJournal.DAL.DTO;
+
+namespace UJournal.DAL.Behavior
+{
+    public interface IStudentService
+    {
+        Task<IEnumerable<StudentDTO>> Get();
+        Task<IEnumerable<StudentDTO>> GetWhere(Expression<Func<StudentDTO, bool>> expression);
+
+        Task<StudentDTO> GetSingle(int id);
+        Task Add(StudentDTO student);
+        Task Update(StudentDTO student);
+        Task Remove(int id);
+    }
+}
---
+++ UJournal.DAL/UJournal.DAL/Service/StudentService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using UJournal.DAL.Behavior;
+using UJournal.DAL.DTO;
+using UJournal.Model.Behavior;
+using UJournal.Model.Models;
+
+namespace UJournal.DAL.Service
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IRepository<Student> repository;
+        private readonly IMapper mapper;
+        public StudentService(IRepository<Student> _repository, IMapper _mapper)
+        {
+            this.repository = _repository;
+            this.mapper = _mapper;
+        }
+
+        public async Task<IEnumerable<StudentDTO>> Get()
+        {
+            IEnumerable<Student> students = await this.repository.Get().ToListAsync<Student>();
+            IEnumerable<StudentDTO> result = this.mapper.Map<IEnumerable<StudentDTO>>(students);
+            return result;
+        }
+
+        public Task<StudentDTO> GetSingle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<StudentDTO>> GetWhere(Expression<Func<StudentDTO, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+        public Task Add(StudentDTO student)
+        {
+            throw new NotImplementedException();
+        }
+        public Task Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(StudentDTO student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
